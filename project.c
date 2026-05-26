2d60f1e0555b7b430028eefd73343f98e5aa8262
turtle847106 <tjhreahv157@gmail.com>
2026-05-26 23:05:47 +0900

    정답 생성 함수 및 입력값 검사 기능 변경
    정답 생성 함수에서 중복되지 않는 랜덤 숫자가 생성되도록 변경하였고,
    입력값 검사 함수를 추가하여 숫자가 아닌 값이 입력될 경우 잘못된 입력으로 처리되도록 변경하였다.
    
    또한 입력받은 문자열을 숫자 배열로 변환하여 게임 진행이 가능하도록 수정하였다.

--- project.c
+++ project.c
@@ -2,75 +2,117 @@
 #include <stdlib.h>
 #include <time.h>
 
-int duplicate(int arr[]) // 숫자가 중복이 되었는지 검사하는 함수
+int duplicate(int arr[]) // 숫자 중복 검사 함수
 {
-    int check[10] = { 0 }; // 0~9
+    int check[10] = { 0 };
     int i;
 
     for (i = 0; i < 4; i++)
     {
         if (check[arr[i]] == 1)
         {
-            return 1; //중복 존재 함수 종료 후 1로 변환
+            return 1; // 중복 존재
         }
 
-        check[arr[i]] = 1; // 현재 숫자가 이미 사용이 되었는지 확인
+        check[arr[i]] = 1;
     }
 
-    return 0;
+    return 0; // 중복 없음
+}
+
+// 정답 생성 함수
+void make_answer(int answer[])
+{
+    int check[10] = { 0 };
+    int i = 0;
+    int num;
+
+    while (i < 4) 
+    {
+        num = rand() % 10; // 0~9 랜덤 숫자 생성
+
+        if (check[num] == 0) // 아직 사용 안 한 숫자라면
+        {
+            check[num] = 1;
+
+            answer[i] = num;// 정답 저장
+
+            i++; // 다음 위치 이동
+        }
+       
+    }
+}
+
+// 입력값 검사 함수
+int invalid_input(char input[])
+{
+    int i;
+
+    for (i = 0; i < 4; i++)
+    {
+        // 숫자 이외 잘못된 입력
+        if (input[i] < '0' || input[i] > '9')
+        {
+            return 0;
+        }
+    }
+
+    return 1; // 정상 입력
 }
 
 int main(void)
 {
     int answer[4], guess[4];
-    int check[10] = { 0 };
     int i, j, strike, ball;
-    int life = 8, count = 0;
+    int life = 7, count = 0;
     char input[5];
 
     srand(time(NULL));
 
-    for (i = 0; i < 4;)
+    // 정답 생성 함수 호출
+    make_answer(answer);
+  
+    for (i = 0; i < 4; i++)
     {
-        int num = rand() % 10;
-
-        if (check[num] == 0) //사용 안 된 숫자 저장
-        {
-            check[num] = 1; // 숫자가 사용되어 
-            answer[i] = num; //num에 저장함
-            i++; // for문 안에 있으면 중복이 있어도 다음 칸으로 이동
-        }
+        printf(" %d", answer[i]);
     }
-
+    printf("\n");
     printf("=== 숫자 야구 게임 ===\n");
+    printf("=== 숫자 4개를 입력하여 정답을 맞추시오 ===\n");
 
     while (life > 0)
     {
         strike = 0;
         ball = 0;
+        
 
         printf("\n숫자 입력 : ");
-        scanf_s("%4s", input, (unsigned)_countof(input)); // 숫자 4개만 입력 받음, input에 배열크기를 알려줌
+        scanf_s("%4s", input, (unsigned)_countof(input));
 
-        for (i = 0; i < 4; i++)
-        {
-            if (input[i] < '0' || input[i] > '9')// 아스키코드 0(48)~9(58) 이외에 잘못된 입력값
-            {
-                break;
-            }
-
-            guess[i] = input[i] - '0'; // 아스키코드 0=48, 3=51 -> 51-48=3
-        }
-
-        if (i != 4 || duplicate(guess))
+        // 입력 검사 함수 호출
+        if (!invalid_input(input))
         {
             printf("잘못된 입력입니다.\n");
             continue;
         }
 
+        // 문자열을 숫자 변환
+        for (i = 0; i < 4; i++)
+        {
+            guess[i] = input[i] - '0'; // 아스키코드 이용한 뺄셈
+        }
+
+        // 중복 검사
+        if (duplicate(guess))
+        {
+            printf("중복된 숫자가 있습니다.\n");
+            continue;
+        }
+
         count++;
 
-        for (i = 0; i < 4; i++) //strike or ball 검사
+        // strike, ball 계산
+        for (i = 0; i < 4; i++) // 이중 for문 수정 
         {
             for (j = 0; j < 4; j++)
             {
@@ -88,6 +130,7 @@ int main(void)
             }
         }
 
+        // 정답 확인
         if (strike == 4)
         {
             printf("정답입니다!\n");
@@ -95,32 +138,35 @@ int main(void)
             break;
         }
 
+        // 결과 출력
         if (ball == 0 && strike == 0)
         {
-            printf("Out\n"); // 입력한 숫자가 하나라도 존재 X
+            printf("Out\n");
         }
         else
         {
-            if (ball > 0) //strike or ball count
+            if (ball > 0)
             {
-                printf("%d Ball ", ball);
+                printf("%dBall ", ball);
             }
 
             if (strike > 0)
             {
-                printf("%d Strike", strike);
+                printf("%dStrike", strike);
             }
 
             printf("\n");
         }
 
         life--;
+
         printf("남은 목숨 : %d\n", life);
     }
 
+    // 게임 오버
     if (life == 0)
     {
-        printf("\n게임 오버!\n정답 : "); //게임오버 시 정답 도출
+        printf("\n게임 오버!\n정답 : ");
 
         for (i = 0; i < 4; i++)
         {
